cfddbd4c457dd493d0d1361ca0f17fa65bec9288
nielsroux <nielsroux@gmail.com>
2022-02-23 09:42:55 +0100

    Added JSX and CSS files Components + config index.js and index.scss

---
+++ src/components/experience/Experience.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './experience.scss';
+
+const Experience = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Experience;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom'
+import App from './App'
+import './style/index.scss'
+
+ReactDOM.render(<App />, document.querySelector("#root"))
